7da81d39eb562656583922e264ebf58b1d525c0f
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2020-12-19 10:49:02 -0500

    Save schema and reports. Keep latest schema in sync with mdb-to-sqlite.sh

---
+++ reports.sql
@@ -0,0 +1,122 @@
+INSERT INTO Report
+(
+  name,
+  description,
+  landscape,
+  input_fields,
+  separate_by,
+  query
+)
+ VALUES
+(
+  'Missing default appointments',
+  'Show all families which do not have a default appointment',
+  1,
+  '',
+  '',
+  'SELECT family_name AS `Family Name`, COALESCE(phone, "") AS Phone, COALESCE(email, "") AS Email FROM Client WHERE length(COALESCE(appt_time_default, '''')) = 0 ORDER BY `Family Name`;'
+);
+
+
+INSERT INTO Report
+(
+  name,
+  description,
+  landscape,
+  input_fields,
+  separate_by,
+  query
+)
+ VALUES
+(
+  'Faulty client data',
+  'Show all families where sum by age does not equal sum by sex',
+  0,
+  '',
+  '',
+  'SELECT family_name FROM Client WHERE count_senior + count_adult + count_child <> count_sex_male + count_sex_female + count_sex_other ORDER BY family_name;'
+);
+
+INSERT INTO Report
+(
+  name,
+  description,
+  landscape,
+  input_fields,
+  subtitle_field,
+  separate_by,
+  query
+)
+ VALUES
+(
+  'Ethnicity monthly report',
+  'Show ethnicity breakdown for a specific distribution',
+  0,
+  '{ "$distribution" : { "type" : "SelectBox", "label" : "Distribution Date"} }',
+  '$distribution',
+  'Ethnicity',
+  'SELECT c.ethnicity AS Ethnicity, COUNT(*) AS Count FROM Client c, Fulfillment f WHERE f.fulfilled AND f.distribution = $distribution AND c.family_name = f.family_name GROUP BY c.ethnicity;'
+);
+
+INSERT INTO Report
+(
+  name,
+  description,
+  landscape,
+  input_fields,
+  subtitle_field,
+  separate_by,
+  query
+)
+ VALUES
+(
+  'Total number of attendees',
+  'Counts of fulfilled clients by category (age, sex, veteran)',
+  1,
+  '{ "$distribution" : { "type" : "SelectBox", "label" : "Distribution Date" } }',
+  '$distribution',
+  '',
+  'SELECT (SUM(count_senior) + SUM(count_adult) + SUM(count_child)) AS "Total", SUM(count_senior) AS Senior, SUM(count_adult) AS Adult, SUM(count_child) AS Child, SUM(count_sex_male) AS Male, SUM(count_sex_female) AS Female, SUM(count_sex_other) AS Other, SUM(count_veteran) AS Veteran FROM Fulfillment f, Client c WHERE f.fulfilled AND f.distribution = $distribution AND c.family_name = f.family_name;'
+);
+
+INSERT INTO Report
+(
+  name,
+  description,
+  landscape,
+  input_fields,
+  subtitle_field,
+  separate_by,
+  query
+)
+ VALUES
+(
+  'Distribution appointments',
+  'Schedule of appointments for a specified distribution',
+  0,
+  '{ "$distribution" : { "type" : "SelectBox", "label" : "Distribution Date" } }',
+  '$distribution',
+  'Time',
+  'SELECT f.appt_day as Day, f.appt_time AS Time, c.family_name as "Family name", c.count_senior + c.count_adult + c.count_child AS "Family size", COALESCE(pet_types, "") AS Pets FROM Fulfillment f, Client c WHERE f.distribution = $distribution AND f.method = "Pick-up" AND c.family_name = f.family_name ORDER BY Day, Time, "Family name";'
+);
+
+INSERT INTO Report
+(
+  name,
+  description,
+  landscape,
+  input_fields,
+  subtitle_field,
+  separate_by,
+  query
+)
+ VALUES
+(
+  'Distribution deliveries',
+  'Required deliveries for a specified distribution',
+  0,
+  '{ "$distribution" : { "type" : "SelectBox", "label" : "Distribution Date" } }',
+  '$distribution',
+  '',
+  'SELECT c.family_name as "Family name", f.delivery_address AS delivery_address, c.count_senior + c.count_adult + c.count_child AS "Family size", COALESCE(pet_types, "") AS Pets FROM Fulfillment f, Client c WHERE f.distribution = $distribution AND f.method = "Delivery" AND c.family_name = f.family_name ORDER BY "Family name";'
+);
---
+++ schema.sql
@@ -0,0 +1,100 @@
+--
+-- Compare with mdb-to-sqlite.sh. Latest is likely there
+--
+
+CREATE TABLE Client
+(
+  family_name       VARCHAR PRIMARY KEY NOT NULL,
+  phone             VARCHAR,
+  email             VARCHAR,
+  ethnicity         VARCHAR,
+  count_senior      INTEGER DEFAULT 0,  -- number of family members 65+
+  count_adult       INTEGER DEFAULT 0,  -- number of family members 18-64
+  count_child       INTEGER DEFAULT 0,  -- number of family members 0-17
+  count_sex_male    INTEGER DEFAULT 0,
+  count_sex_female  INTEGER DEFAULT 0,
+  count_sex_other   INTEGER DEFAULT 0,
+  count_veteran     INTEGER DEFAULT 0,
+  income_source     VARCHAR,
+  income_amount     REAL,
+  pet_types         VARCHAR,
+  address_default   VARCHAR, -- default address for delivery
+  appt_day_default  INTEGER, -- default appt day, 0-relative to Distr start
+  appt_time_default VARCHAR, -- default appt time HH:MM,
+  verified          BOOLEAN DEFAULT FALSE,
+  archived          BOOLEAN DEFAULT FALSE,
+  UNIQUE (family_name COLLATE NOCASE)
+);
+
+INSERT INTO Client
+      (family_name, phone, email, ethnicity, verified,
+       count_senior, count_adult, count_child,
+       count_sex_male, count_sex_female, count_sex_other, count_veteran,
+       income_source, income_amount,
+       pet_types, appt_day_default, appt_time_default)
+  SELECT
+      `Family Name`, `Phone number`, Email, Ethnicity, Verified,
+      `65+`, `18+`, `0-18`,
+      Male, Female, `Undefined Gender`, Veteran,
+      `Income source`, `Income Amount`,
+      Pets, 1, strftime('%H:%M', `Appt time`)
+    FROM Clients;
+
+-- We no longer need the original table we imported from
+DROP TABLE Clients;
+
+
+CREATE TABLE Fulfillment
+(
+  distribution      VARCHAR REFERENCES DistributionPeriod
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
+  family_name       VARCHAR REFERENCES Client
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
+  appt_day          INTEGER,    -- 0-relative to distribution
+  appt_time         VARCHAR,    -- %H:%M; null if fullfilled w/out appointment
+  method            VARCHAR,    -- one of "Pick-up" or "Delivery"
+  delivery_address  VARCHAR,
+  fulfilled         BOOLEAN DEFAULT FALSE,  -- has been picked up or delivered
+  fulfillment_time  VARCHAR,                -- %Y-%m-%d %H:%M:%S
+  PRIMARY KEY (distribution, family_name)
+);
+
+CREATE INDEX Fulfillment_appt_idx
+  ON Fulfillment(distribution, appt_day, appt_time);
+
+
+CREATE TABLE DistributionPeriod
+(
+  start_date        VARCHAR PRIMARY KEY,    -- %Y-%m-%d
+
+  -- This might later change to a separate table of appointment
+  -- start/end per day, if we need more than one week.
+  day_1_first_appt  VARCHAR,    -- %H:%M
+  day_1_last_appt   VARCHAR,
+  day_2_first_appt  VARCHAR,
+  day_2_last_appt   VARCHAR,
+  day_3_first_appt  VARCHAR,
+  day_3_last_appt   VARCHAR,
+  day_4_first_appt  VARCHAR,
+  day_4_last_appt   VARCHAR,
+  day_5_first_appt  VARCHAR,
+  day_5_last_appt   VARCHAR,
+  day_6_first_appt  VARCHAR,
+  day_6_last_appt   VARCHAR,
+  day_7_first_appt  VARCHAR,
+  day_7_last_appt   VARCHAR
+);
+
+
+CREATE TABLE Report
+(
+  name              VARCHAR PRIMARY KEY NOT NULL,
+  description       VARCHAR NOT NULL,
+  query             VARCHAR NOT NULL,
+  input_fields      VARCHAR,
+  separate_by       VARCHAR,
+  landscape         BOOLEAN DEFAULT 0,
+  UNIQUE (name COLLATE NOCASE)
+);
