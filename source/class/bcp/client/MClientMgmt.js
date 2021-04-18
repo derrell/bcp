3da141da1332f09f6216d3b91c21a64713843c89
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2021-04-18 09:49:36 -0400

    income_amount is REAL in db; need to ensure it's text in gui textfield

--- source/class/bcp/client/MClientMgmt.js
+++ source/class/bcp/client/MClientMgmt.js
@@ -633,7 +633,7 @@ qx.Mixin.define("bcp.client.MClientMgmt",
           {
             type       : "TextField",
             label      : "Income amount",
-            value      : clientInfo.income_amount || "",
+            value      : (clientInfo.income_amount || "").toString(),
             properties :
             {
               tabIndex   : 7
